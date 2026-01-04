14bb0f0d3530a33c419b7c640c24e5e610cff241
Swastika <swastikadhakal37@gmail.com>
2026-01-04 22:48:09 +0545

    just background

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import Grocery from '../../assets/grocery.png'
+
+const Hero = () => {
+  return (
+    <section className="py-16">
+      <div className="max-w-[1400px] mx-auto px-10 grid md:grid-cols-2 items-center gap-10">
+        
+        {/* Text Content */}
+        <div className="space-y-6">
+          <span className="bg-orange-100 text-orange-500 text-sm px-5 py-2 rounded-full">
+            Export Best Quality....
+          </span>
+
+          <h1 className="text-4xl md:text-5xl font-bold">
+           Tasty Organic  <span className="text-orange-500">Fruits & Veggies </span>
+         In Your City
+ </h1>
+         <p className="max-w-md text-gray-600 text-[18px] leading-8 font-['Dancing_Script']">
+                We provide fresh, organic, and healthy groceries directly from farms
+                to your doorstep — quality you can taste, freshness you can trust.
+              </p>
+
+
+        </div>
+
+        {/* Image */}
+        <div className="flex justify-center">
+          <img
+            src={Grocery}
+            alt="Hero Image"
+            className="w-[80%] max-w-md"
+          />
+        </div>
+
+      </div>
+    </section>
+  )
+}
+
+export default Hero
