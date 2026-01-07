587f27738bb0f20b8103b534cf2e8a065f5fe333
Swastika <swastikadhakal37@gmail.com>
2026-01-07 23:07:12 +0545

    Product grid

--- src/components/Discount/Discount.jsx
+++ src/components/Discount/Discount.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
+import React from 'react';
+import Button from '../Button/Button';
 
 const Discount = () => {
   return (
-    <section class='bg-zinc-100'>
-        <div className='py-10'>
-            <span>
-                <h3> First Order discount</h3>
-                <p>
-                    Enjoy an exclusive first order discount on our grocery website! Shop fresh essentials and save big on your first purchase. Fast delivery and quality guaranteed
-                </p>
-            </span>
-
-        </div>
+    <section className='bg-zinc-100 relative py-10 overflow-hidden'>
+      {/* Rotated Badge */}
+    
+      <div className='max-w-2xl mx-auto text-center'>
+        <span className="text-9xl"></span>
+        <h3 className='text-3xl font-bold mb-4'>First Order Discount</h3>
+        <p className='text-gray-600 mb-6'>
+          Enjoy an exclusive first order discount on our grocery website! Shop fresh essentials and save big on your first purchase. Fast delivery and quality guaranteed.
+        </p>
+        <Button content="Get a Discount" />
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Discount
+export default Discount;
